ef3794c3429d0f576413312ce775f3d543e0729d
Ducvoccer <ntduc250798@gmail.com>
2021-03-15 03:46:09 +0700

    change binnary paht

--- ncov.py
+++ ncov.py
@@ -9,7 +9,7 @@ CHROMEDRIVER_PATH = './chromedriver'
 chrome_options = Options()
 
 chrome_options.add_argument("--headless")
-chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
+# chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
 
 browser = webdriver.Chrome(
     executable_path=CHROMEDRIVER_PATH, options=chrome_options)
