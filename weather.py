dd277bb7ead2994a381ffa02faa1f761ad43b867
Ducvoccer <ntduc250798@gmail.com>
2021-05-25 02:38:42 +0700

    change api key weather

--- weather.py
+++ weather.py
@@ -1,14 +1,17 @@
 # -*- coding: UTF-8 -*-
 import requests
+from requests.api import get
 
 api_key = '2479a2376552133d8de7d0d9dca046f4'
 
 def get_weather(location):
     location = location
-    api_address = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=6dcccf305d39905f92566e9e23a20c49'.format(location)
+    api_address = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(location, api_key)
     json_data = requests.get(api_address).json()
     if json_data['cod'] == 200:
         return "Thời Tiết ở {0} là: {1}\nNhiệt độ thấp nhất: {2}°C\nNhiệt độ cao nhất: {3}°C".format(json_data['name'], json_data['weather'][0]['main'], int(json_data['main']['temp_min']-273), int(json_data['main']['temp_max']-273))
     else:
         return 'Tên thành phố không hợp lệ'
 
+
+print(get_weather('hà nội'))
